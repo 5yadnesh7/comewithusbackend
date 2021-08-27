6987fccf5688d296068951053ec83ecc81b3c984
5yadnesh7 <65652731+5yadnesh7@users.noreply.github.com>
2021-08-27 16:57:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const { default: axios } = require("axios");
+const cheerio = require("cheerio");
+const fbviddown = require("fb-video-downloader");
+const ytdl = require("ytdl-core");
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 2121
+
+app.use(
+    cors({
+        origin: ["http://localhost:3000"],
+        methods: ["GET", "POST"],
+        credentials: true,
+    })
+);
+app.use(bodyParser.json());
+
+// Instagram Video Download
+app.post('/insta', async function (req, res) {
+    const { videourl } = req.body
+    const data = await axios.get(videourl)
+    const resp = await data.data
+    const $ = cheerio.load(resp);
+    // const img = $('meta[property="og:image"]').attr("content")
+    const videos = $('meta[property="og:video"]').attr("content")
+    if (videos === undefined) {
+        res.send("Invalid Video Url")
+    } else {
+        res.send(videos)
+    }
+});
+// Facebook Video Download
+app.post('/facebk', async function (req, res) {
+    const { videourl } = req.body
+    try {
+        const data = await fbviddown.getInfo(videourl);
+        res.send(data);
+    } catch (e) {
+        res.send("Error to download facebook Video")
+    }
+});
+// Facebook Video Download
+app.post('/youtu', async function (req, res) {
+    const { videourl } = req.body
+    try {
+        const info = await ytdl.getInfo(videourl);
+        res.send(info);
+    } catch (e) {
+        res.send("Error to download youtube Video")
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Serve is running on ${PORT}`)
+})
